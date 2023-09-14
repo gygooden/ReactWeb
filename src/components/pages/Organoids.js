0b77ae443c952128d686aba97445d7488b338be4
Grayson <gygooden@gmail.com>
2023-09-14 11:15:53 -0400

    organoids page w/practice videos

--- src/components/pages/Organoids.js
+++ src/components/pages/Organoids.js
@@ -2,31 +2,37 @@ import React from 'react';
 import '../../App.css';
 import '../Organoids.css';
 
+const organoidVideos = [
+  'https://www.youtube.com/embed/0I5MldVOjWg',
+  'https://www.youtube.com/embed/0I5MldVOjWg',
+  'https://www.youtube.com/embed/0I5MldVOjWg',
+  'https://www.youtube.com/embed/0I5MldVOjWg',
+];
+
 export default function Organoids() {
   return (
     <div>
-    <h1 className='mission'>ORGANOIDS</h1>;   
-    <div className='organoids-container'>
-      <p className='organoids-description'>
-      Organoids are “mini-organs” in a dish. Until now, a lack of vascularization has limited human organoid viability & growth.
-      We create scalable & reproducible heart & liver vascularized organoids, and will eventually create all organ types in integrated systems.
-      </p>
-      <div className='organoid-videos'>
-        {/* You can replace the following placeholders with your organoid video components */}
-        <div className='organoid-video'>
-          {/* Video 1 */}
-        </div>
-        <div className='organoid-video'>
-          {/* Video 2 */}
-        </div>
-        <div className='organoid-video'>
-          {/* Video 3 */}
-        </div>
-        <div className='organoid-video'>
-          {/* Video 4 */}
+      <h1 className='mission'>ORGANOIDS</h1>
+      <div className='organoids-container'>
+        <p className='organoids-description'>
+          Organoids are “mini-organs” in a dish. Until now, a lack of vascularization has limited human organoid viability & growth.
+          We create scalable & reproducible heart & liver vascularized organoids, and will eventually create all organ types in integrated systems.
+        </p>
+        <div className='organoid-videos'>
+          {organoidVideos.map((videoSrc, index) => (
+            <div className='organoid-video' key={index}>
+=              <iframe
+                title={`Organoid Video ${index + 1}`}
+                width='100%'
+                height='200'
+                src={videoSrc}
+                frameBorder='0'
+                allowFullScreen
+              ></iframe>
+            </div>
+          ))}
         </div>
       </div>
-    </div>
     </div>
   );
 }
